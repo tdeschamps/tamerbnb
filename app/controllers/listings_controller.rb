0ff767e80dda05cdcfd860905022676b9a3ed7f4
tdeschamps <thomasdscmps@gmail.com>
2014-05-19 13:35:23 +0200

    Add user and listing controllers

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,19 @@
+class ListingsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
